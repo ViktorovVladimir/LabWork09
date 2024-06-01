9401a869630ef4577930f8e9b71b3a20a54e724e
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-06-01 23:39:20 +0300

    ex.1 was complete
    - выполнил Упражнение №1 "Разработка класса Book"

---
+++ Lab10/Starter/MyClass/MyClass/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+//--.
+class Book
+{
+    //--.
+    String author;      //автор
+    String title;       //название
+    String publisher;   //издательство
+    int pages;          //кол-во страниц
+    int year;           //год издания
+
+    //--.
+    static double price = 9;
+
+    //--.
+    public Book(String author, String title, String publisher, int pages, int year)
+    {
+        this.author = author;
+        this.title = title;
+        this.publisher = publisher;
+        this.pages = pages;
+        this.year = year;
+    }
+
+    //--.
+    public Book()           //статический конструктор
+    {
+        price = 10;
+    }
+
+    //--.
+    public static void SetPrice(double price)
+    {
+        Book.price = price;
+    }
+
+    //--.
+    public void Show()
+    {
+        Console.WriteLine("\n Книга: \n Автор: {0}\n Название: {1}\n Год издания: {2}\n {3} стр.\n Стоимость аренды: {4}", author, title, year, pages, Book.price);
+    }
+
+    //--.
+    public double PriceBook(int s)
+    {   
+        double cust = s * price;
+        return cust;
+    }   
+
+}
