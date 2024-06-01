8f229e68a55925e98337e8a6a5721b6b7ba08dc5
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-06-01 23:46:37 +0300

    -add ex.2 implementation of class inheritance
    - добавил программу для выполнения Упражнения №2 Реализация наследования классов

---
+++ Lab10/Starter/BookEx2/BookEx2/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyClassNameSpace
+{
+    class Test
+    {
+        //--.
+        static void Main()
+        {
+            //--.
+            Book b2 = new Book("Толстой Л.Н.", "Война и мир", "Наука", 823, 2013);
+            b2.Show();
+            Book.SetPrice(12);
+            b2.Show(); 
+        }
+
+    }
+}
